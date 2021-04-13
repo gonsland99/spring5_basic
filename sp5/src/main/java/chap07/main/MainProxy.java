1744c621833fc149d5342095299543b6168b364d
kimbyunggon <62224768+kimbyunggon@users.noreply.github.com>
2021-04-13 21:35:22 +0900

    AOP를 사용하는 이유

--- sp5/src/main/java/chap07/main/MainProxy.java
+++ sp5/src/main/java/chap07/main/MainProxy.java
@@ -7,6 +7,7 @@ import chap07.aop.RecCalculator;
 public class MainProxy {
 
 	public static void main(String[] args) {
+		//미리초를 나노초로 바꾸고자 할때 수정대신 ExeTimeCaclulator를 이용하여 분리 재사용성 증가(aop방식)
 		ExeTimeCalculator ttCal1 = new ExeTimeCalculator(new ImpeCalculator());
 		System.out.println(ttCal1.factorial(20));
 
